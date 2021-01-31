44f536f25814bf600e79bb513cd4a1d60d91394b
crusopaul <crusopaul@gmail.com>
2021-01-31 11:47:43 -0500

    Make it run

---
+++ src/main/java/io/github/crusopaul/OreRandomizer/compat/exception/BadVersion.java
@@ -0,0 +1,7 @@
+package io.github.crusopaul.OreRandomizer.compat.exception;
+
+public class BadVersion extends Exception {
+  public BadVersion(String errorMessage) {
+    super(errorMessage);
+  }
+}
