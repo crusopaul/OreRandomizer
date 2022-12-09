978f3a1c0d019ac90cdc128ffa56b8219bdc6046
crusopaul <crusopaul@gmail.com>
2022-12-08 19:12:03 -0500

    Add support for 1.19.3

--- src/main/java/io/github/crusopaul/OreRandomizer/compat/VersionEngine.java
+++ src/main/java/io/github/crusopaul/OreRandomizer/compat/VersionEngine.java
@@ -60,7 +60,8 @@ public class VersionEngine {
   private Version parseVersionString(String BukkitVersionString) {
     final Version version;
 
-    if (BukkitVersionString.contains("1.19.2")
+    if (BukkitVersionString.contains("1.19.3")
+        || BukkitVersionString.contains("1.19.2")
         || BukkitVersionString.contains("1.19.1")
         || BukkitVersionString.contains("1.19")) {
       version = Version.Frogs;
